c39b7cb7a6142b65755276a43c668082919c7fc4
manish8285 <ermaanish@gmail.com>
2024-09-12 16:32:02 +0530

    initial commit

---
+++ src/demos/RestaurantLandingPage.js
@@ -0,0 +1,137 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import Features from "components/features/ThreeColSimple.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+import DownloadApp from "components/cta/DownloadApp.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+
+import chefIconImageSrc from "images/chef-icon.svg";
+import drugDelhivery from 'images/drug-delivery_602838-525.avif'
+import medicineOrdering from 'images/medicine_ordering.avif'
+import celebrationIconImageSrc from "images/celebration-icon.svg";
+import shopIconImageSrc from "images/shop-icon.svg";
+
+export default () => {
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
+  return (
+    <AnimationRevealPage>
+      <Hero
+        heading={<>Get Your Medicine<HighlightedText>From Nearby Store.</HighlightedText></>}
+        description="Getting medicine is now easier than ever! Simply upload your prescription, and the nearest store will quickly deliver your medicine right to your door—no need to go from store to store."
+        imageSrc={drugDelhivery}
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        primaryButtonText="Upload Prescription"
+        watchVideoButtonText="Download App"
+      />
+      <MainFeature
+        subheading={<Subheading>Your Medicine is</Subheading>}
+        heading={
+          <>
+            Just One Click Away
+            <wbr /> <HighlightedText>Get Up to 40% Off!</HighlightedText>
+          </>
+        }
+        description={
+          <Description>
+            Your medicine is just a click away. Enjoy up to 40% savings on all your medications, with full support from licensed pharmacists. Say goodbye to high costs and get the care you need, hassle-free!
+            <br />
+            <br />
+          </Description>
+        }
+        buttonRounded={false}
+        textOnLeft={false}
+        primaryButtonText="Latest Offers"
+        imageSrc={
+          medicineOrdering
+        }
+        imageCss={imageCss}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
+      />
+      {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
+      <TabGrid
+        heading={
+          <>
+            All Your Needs <HighlightedText>At One Place.</HighlightedText>
+          </>
+        }
+      />
+      <Features
+        heading={
+          <>
+            Amazing <HighlightedText>Services.</HighlightedText>
+          </>
+        }
+        cards={[
+          {
+            imageSrc: shopIconImageSrc,
+            title: "1030+ Pharmacies",
+            description: "All Pharmacies in One Place – No Need to Go Anywhere!",
+            url: "https://google.com"
+          },
+          {
+            imageSrc: chefIconImageSrc,
+            title: "Licensed Pharmacists",
+            description: "Get Your Medicine from Trusted Pharmacists",
+            url: "https://timerse.com"
+          },
+          {
+            imageSrc: celebrationIconImageSrc,
+            title: "Birthday Catering",
+            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            url: "https://reddit.com"
+          }
+        ]}
+
+        imageContainerCss={tw`p-2!`}
+        imageCss={tw`w-20! h-20!`}
+      />
+      <MainFeature2
+        subheading={<Subheading>A Reputed Brand</Subheading>}
+        heading={<>Why <HighlightedText>Choose Us ?</HighlightedText></>}
+        statistics={[
+          {
+            key: "Orders",
+            value: "94000+",
+          },
+          {
+            key: "Customers",
+            value: "11000+"
+          },
+          {
+            key: "Stores",
+            value: "1030+"
+          }
+        ]}
+        primaryButtonText="Order Now"
+        primaryButtonUrl="https://order.now.com"
+        imageInsideDiv={false}
+        imageSrc="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEzNzI2fQ&auto=format&fit=crop&w=768&q=80"
+        imageCss={Object.assign(tw`bg-cover`, imageCss)}
+        imageContainerCss={tw`md:w-1/2 h-auto`}
+        imageDecoratorBlob={true}
+        imageDecoratorBlobCss={tw`left-1/2 md:w-32 md:h-32 -translate-x-1/2 opacity-25`}
+        textOnLeft={true}
+      />
+      <Testimonial
+        subheading=""
+        heading={<>Customers <HighlightedText>Love Us.</HighlightedText></>}
+      />
+      <DownloadApp
+        text={<>People around you are ordering delicious meals using the <HighlightedTextInverse>Treact App.</HighlightedTextInverse></>}
+      />
+      <Footer />
+    </AnimationRevealPage>
+  );
+}
